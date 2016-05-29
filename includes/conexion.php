402a67ecd41fe09056c66d712b9bc661ee5a4b39
Valentin Mari <valentinmari@hotmail.com>
2016-05-29 19:38:57 -0300

    supresión de errores para php

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,6 +1,6 @@
 <?php 
 session_start();
-error_reporting('E_NONE');
+error_reporting(E_ALL & ~(E_STRICT|E_NOTICE));
 $conexion = new mysqli('localhost','couchinn','FZxMQCESvfwDPyQC','couchinn');
 $conexion->set_charset("utf8");
 
