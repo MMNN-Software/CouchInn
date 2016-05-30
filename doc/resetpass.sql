09f08020116759aa6fe35a349dde081244740413
matidiaz95 <matidiaz95@gmail.com>
2016-05-29 21:16:59 -0300

    Add files via upload

---
+++ doc/resetpass.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 30-05-2016 a las 02:11:39
+-- Versión del servidor: 10.1.9-MariaDB
+-- Versión de PHP: 5.6.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `couchinn`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `resetpass`
+--
+
+CREATE TABLE `resetpass` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `idusuario` varchar(255) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `creado` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `resetpass`
+--
+ALTER TABLE `resetpass`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `idusuario` (`idusuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `resetpass`
+--
+ALTER TABLE `resetpass`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
