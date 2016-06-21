79ac0fbc5981601d6bc3ff7ccf4353fcd70308d4
mgfeatherston <mgfeatherston@users.noreply.github.com>
2016-06-21 18:29:49 -0300

    Validacion en el popup (Mejorar Cuenta).

--- includes/form_premium.php
+++ includes/form_premium.php
@@ -13,20 +13,20 @@
               <div class="col-xs-12">
                 <label class="control-label">Número de tarjeta</label>
                 <div class="input-group">
-                  <input type="text"  name="tarjeta" class="form-control" placeholder="XXXXXXXXXXXXXXXX" minlength="16" maxlength="16" required autofocus autocomplete="off">
+                  <input type="text"  name="tarjeta" class="form-control" onkeypress="validateNumberField(event)" placeholder="XXXXXXXXXXXXXXXX" minlength="16" maxlength="16" required autofocus autocomplete="off" pattern="^[0-9]{16}$" title="El numero de tarjeta debe ser de 16 digitos." />
                   <span class="input-group-addon"><span class="glyphicon glyphicon-credit-card"></span></span>
                 </div>
               </div>
               <div class="col-xs-6">
                 <div class="form-group">
                   <label class="control-label">Fecha de vencimiento</label>
-                  <input type="text"  name="expires" class="form-control" placeholder="MM/AA" minlength="5" maxlength="5" required autocomplete="off">
+                  <input type="text" pattern="^([01][0-2]|0?[1-9])\/[1-5][0-9]$" name="expires" class="form-control" placeholder="MM/AA" minlength="5" maxlength="5" required autocomplete="off" title="La fecha de vencimiento debe ser posterior a hoy." />
                 </div>
               </div>
               <div class="col-xs-6">
                 <div class="form-group">
                   <label class="control-label">Código de seguridad</label>
-                  <input type="text"  name="ccv" class="form-control" placeholder="XXX" required autocomplete="off" minlength="3" maxlength="4">
+                  <input type="text" pattern="^[0-9]{3,4}$" onkeypress="validateNumberField(event)" name="ccv" class="form-control" placeholder="XXX" required autocomplete="off" minlength="3" maxlength="4" title="El codigo de seguridad debe ser de 3 digitos." />
                 </div>
               </div>
             </div>
@@ -40,3 +40,15 @@
     </div>
   </form>
 </div>
+<script type="text/javascript">
+function validateNumberField(evt) {
+  var theEvent = evt || window.event;
+  var key = theEvent.keyCode || theEvent.which;
+  key = String.fromCharCode( key );
+  var regex = /[0-9]|\./;
+  if( !regex.test(key) ) {
+    theEvent.returnValue = false;
+    if(theEvent.preventDefault) theEvent.preventDefault();
+  }
+}
+</script>
