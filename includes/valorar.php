77561bebd162353ccde9b2fd9214125d3e87adea
matidiaz95 <matidiaz95@gmail.com>
2016-06-24 17:13:02 -0300

    Add files via upload

--- includes/valorar.php
+++ includes/valorar.php
@@ -1,30 +1,32 @@
-<div class="panel panel-default">
-  <div class="panel-body">
-    <h5>Valoraciones</h5>
-    <hr>
-  </div>
-  
-  
- <?php
+<?php
 include 'includes/conexion.php';
 include 'includes/header.php';
 $iduser = $_SESSION['id'];
 
 
-$publi = $conexion->query("SELECT * FROM publicacion where id IN (SELECT publicacion_id FROM reserva WHERE estado = 2 AND usuario_id = $iduser AND hasta > CURDATE())");
+$publi = $conexion->query("SELECT id FROM publicacion where NOT IN (SELECT publicacion_id FROM reserva WHERE estado = 2 AND $iduser = $iduser)");
+
+$valorar = $conexion->query("SELECT * FROM reserva WHERE estado = 2 AND $iduser = $iduser");
 $hoy = date("Y-m-d");
 
-?>
-<?php
-if ($publi->num_rows > 0) {
-	   $val_res = array();
-	   while ( $val = $publi->fetch_assoc()){
-		   	$val_res[] = $val;  
-	   }
- ?>
-    <div class="list-group">
-<?php foreach ($val_res as $pub) { ?>
 
+if ($valorar->num_rows > 0) {
+	   $val_res = array();
+	   while ( $val = $valorar->fetch_assoc()){
+		   if ($val['fecha'] > $hoy){
+				$val_res[] = $val;
+		   }		   
+	   }
+   }
+
+foreach ($val_res AS $val): 
+?>
+<div class="panel panel-default">
+  <div class="panel-body">
+    <h5>Valorar reservas</h5>
+    <hr>
+    <div class="list-group">
+<?php while( $pub = $publi->fetch_assoc() ){ ?>
       <div class="list-group-item clearfix">
         <span class="pull-left"><?php $imagenes = $conexion->query("SELECT path FROM imagen WHERE publicacion_id = '{$pub['id']}' ORDER BY orden ASC LIMIT 1"); 
         if($imagenes->num_rows){
@@ -34,21 +36,18 @@ if ($publi->num_rows > 0) {
           <img src="/img/logo-pub.png"  style="width:150px;margin-right:10px">
         <?php } 
         $imagenes->free(); ?></span>
-		<span class="pull-right">
-          <?php include 'includes/confirmar_valoracion.php';?>		
-        
-           <h4><button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-target="#valorar" >Valorar</button></h4>
-               
-        </span>		
         <h4 class="list-group-item-heading"><a href="/Publicacion.php?id=<?php echo $pub['id'] ?>"><?php echo $pub['titulo']; ?></a></h4>
         <!--<p class="list-group-item-text"><span class="glyphicon glyphicon-comment"></span> </p>-->
       </div>
+<?php } if(!$publicaciones->num_rows):?>
+  <p class="text-center">No se han encontrado publicaciones.</p>
+<?php endif ?>
     </div>
-	
- <?php 
- }
- 
- }
- ?> 
-
+  </div>
 </div>
+
+	  <?php
+endforeach
+
+
+?>
