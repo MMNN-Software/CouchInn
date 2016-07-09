f8b998df55b1861395789f300f8f97beb0a4b8b2
mgfeatherston <mgfeatherston@users.noreply.github.com>
2016-07-09 17:04:12 -0300

    Acerca de...

--- Acerca.php
+++ Acerca.php
@@ -3,8 +3,20 @@
   include 'includes/header.php';
   include 'includes/functions.php';
 ?>
-
-<div style="text-align:center">
-<img src="/img/WorkInProgress.png" title="WorkInProgress" style="width:60%; text-align:center" />
-<div>
+<div class="container main">
+	<div class="col-sm-12">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+			<h5>Acerca de Couchinn.com</h5>
+			<p>Couchinn.com se inicio hace varios años ya como un blog donde se publicabamos hospedajes ofertados, con la finalidad de que las personas pudieran organizar las estadias de sus viejas de una forma comoda.</p>
+			<p>Hoy, mejoramos nuestro servicio pasando de ser un simple blog a una aplicación web, que les permitirá a nuestros usuarios no solo encontrar sus futuros hospedajes sino tambien realizar una reserva más confiable.</p>
+			<p>Esperamos que les guste y se sumen a nuestra comunidad, ya sea ofertando nuevos hospedajes o bien reservandolos.</p>
+			<p>Coucchin.com</p>
+			<div style="text-align:center">
+				<img src="/img/logoc.png" title="WorkInProgress" style="width:60%; text-align:center" />
+			</div>
+		</div>
+	  </div>
+	</div>
+</div>
 <?php include 'includes/footer.php'; ?>
