712ad3457d444a3774e7b9b51ca3b889c9d3bd34
Valentin Mari <valentinmari@hotmail.com>
2016-07-09 18:32:42 -0300

    arreglo

--- Acerca.php
+++ Acerca.php
@@ -6,14 +6,15 @@
 <div class="container main">
 	<div class="col-sm-12">
       <div class="panel panel-default">
-        <div class="panel-heading">
+        <div class="panel-body">
 			<h5>Acerca de Couchinn.com</h5>
+			<hr>
 			<p>Couchinn.com se inicio hace varios años ya como un blog donde se publicabamos hospedajes ofertados, con la finalidad de que las personas pudieran organizar las estadias de sus viejas de una forma comoda.</p>
 			<p>Hoy, mejoramos nuestro servicio pasando de ser un simple blog a una aplicación web, que les permitirá a nuestros usuarios no solo encontrar sus futuros hospedajes sino tambien realizar una reserva más confiable.</p>
 			<p>Esperamos que les guste y se sumen a nuestra comunidad, ya sea ofertando nuevos hospedajes o bien reservandolos.</p>
 			<p>Coucchin.com</p>
 			<div style="text-align:center">
-				<img src="/img/logoc.png" title="WorkInProgress" style="width:60%; text-align:center" />
+				<img src="/img/logoc.png" style="width:60%; text-align:center" />
 			</div>
 		</div>
 	  </div>
